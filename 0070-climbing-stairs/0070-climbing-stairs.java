d2b18fdd651be587c2eb4aabcd17993869491172
jaya-chaitanya-surthineni <surthinenijayachaitanya@gmail.com>
2026-08-18 07:39:33 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.76%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2)return n;
+        int first=1,second=2;
+        for(int i=3;i<=n;i++){
+            int cur=first+second;
+            first=second;
+            second=cur;
+        }
+    return second;
+    }
+}
