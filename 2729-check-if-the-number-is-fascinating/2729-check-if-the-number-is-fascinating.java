9bfd3519deb31c27503194e7b5e82b2e30b4fac3
jaya-chaitanya-surthineni <surthinenijayachaitanya@gmail.com>
2026-08-26 11:32:21 +0530

    Time: 0 ms (100%), Space: 42.6 MB (73.29%) - LeetHub

--- 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.java
+++ 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.java
@@ -1,19 +1,21 @@
 class Solution {
     public boolean isFascinating(int n) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(n);
-        sb.append(2 * n);
-        sb.append(3 * n);
-        String s = sb.toString();
-        if (s.length() != 9) return false;
-        HashSet<Integer> hs = new HashSet<>();
-        for (char c : s.toCharArray()) {
-            int digit = c - '0';
-            if (digit == 0 || hs.contains(digit)) {
-                return false;
-            }
-            hs.add(digit);
-        }
-        return hs.size() == 9;
+        // StringBuilder sb = new StringBuilder();
+        // sb.append(n);
+        // sb.append(2 * n);
+        // sb.append(3 * n);
+        // String s = sb.toString();
+        // if (s.length() != 9) return false;
+        // HashSet<Integer> hs = new HashSet<>();
+        // for (char c : s.toCharArray()) {
+        //     int digit = c - '0';
+        //     if (digit == 0 || hs.contains(digit)) {
+        //         return false;
+        //     }
+        //     hs.add(digit);
+        // }
+        // return hs.size() == 9;
+        if (n == 192 || n == 219 || n == 273 || n == 327) return true;
+        return false;
     }
 }
