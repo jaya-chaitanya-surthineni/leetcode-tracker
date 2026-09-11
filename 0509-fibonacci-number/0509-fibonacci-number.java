6eae97b181648eb8b408d3c9c8e7c240d681561c
jaya-chaitanya-surthineni <surthinenijayachaitanya@gmail.com>
2026-09-11 11:43:52 +0530

    Time: 11 ms (9.17%), Space: 42.1 MB (52.2%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,11 +1,14 @@
 class Solution {
     public int fib(int n) {
-        int a=0,b=1;
-        if(n<=1)return n;
-        for(int i=2;i<=n;i++){
-            int c=a+b;
-            a=b;b=c;
-        }
-    return b;
+    //     int a=0,b=1;
+    //     if(n<=1)return n;
+    //     for(int i=2;i<=n;i++){
+    //         int c=a+b;
+    //         a=b;b=c;
+    //     }
+    // return b;
+    if(n==0)return 0;
+    if(n==1)return 1;
+    return fib(n-1)+fib(n-2);
     }
 }
